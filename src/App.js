d8ba2bf7c79f17bedd1e4569622d388ee28272e5
akshaypoori <58963518+akshaypoori@users.noreply.github.com>
2025-05-15 20:30:50 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ImageCompressor from './components/ImageCompressor';
+import';
+
+function App() {
+    return (
+        <div>
+            <h1>File Compressor</h1>
+            <ImageCompressor />
+            <PdfCompressor />
+        </div>
+    );
+}
+
+export default App;
